c3e60dc0a7086299c2426115c62002d2e3049652
Julia <riyamtechcampus@gmail.com>
2020-06-19 15:38:01 +0300

    setup paypal in the store

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -27,7 +27,7 @@ export default class Cart extends Component {
                   </div>
                   <CartColumns />
                   <CartList value={value} />
-                  <CartTotals value={value} />
+                  <CartTotals value={value} history={this.props.history} />
                 </React.Fragment>
               );
             } else {
